f58915672cae09e3c243b20cff7be579ca4e85d2
Zbigniew Oględzki <zgibek@gmail.com>
2020-11-07 05:24:53 +0100

    adds reading default credentials from environment variables first

--- src/main/java/com/smockin/admin/service/utils/aws/AwsCredentialsProvider.java
+++ src/main/java/com/smockin/admin/service/utils/aws/AwsCredentialsProvider.java
@@ -15,10 +15,30 @@ public class AwsCredentialsProvider {
     private final Map<String, String> credentials = new HashMap<>();
     private final AwsProfile defaultProfile;
 
+    private static final String AWS_ACCESS_KEY_ID = "AWS_ACCESS_KEY_ID";
+    private static final String AWS_SECRET_ACCESS_KEY = "AWS_SECRET_ACCESS_KEY";
+    private static final String AWS_DEFAULT_REGION = "AWS_DEFAULT_REGION";
+
     public AwsCredentialsProvider(AwsProfileProvider awsProfileProvider) throws IOException {
-        this.defaultProfile = awsProfileProvider.findDefaultProfile();
-        if (defaultProfile != null) {
+        // check environment variables first
+        final String awsAccessKeyFromEnv = System.getenv(AWS_ACCESS_KEY_ID);
+        final String awsSecretAccessKeyFromEnv = System.getenv(AWS_SECRET_ACCESS_KEY);
+        final String awsRegionFromEnv = System.getenv(AWS_DEFAULT_REGION);
+
+        if (awsAccessKeyFromEnv != null && awsSecretAccessKeyFromEnv != null && awsRegionFromEnv != null) {
+            this.defaultProfile = new AwsProfile("default",
+                    awsAccessKeyFromEnv, awsSecretAccessKeyFromEnv,
+                    awsRegionFromEnv
+            );
             add(defaultProfile);
+            logger.info("Loaded credentials data for [" + awsAccessKeyFromEnv + "] from environment variables");
+        } else {
+
+            // then, if not found in env variables, check credentials file
+            this.defaultProfile = awsProfileProvider.findDefaultProfile();
+            if (defaultProfile != null) {
+                add(defaultProfile);
+            }
         }
     }
 
