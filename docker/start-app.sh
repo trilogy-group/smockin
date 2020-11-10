ea7f82dddde959cd7ac46dd2c15bf327e2f1e8b3
Zbigniew Oględzki <zgibek@gmail.com>
2020-11-10 18:52:21 +0100

    review fixes, adapting to openjdk:11 image

--- docker/start-app.sh
+++ docker/start-app.sh
@@ -2,7 +2,7 @@ if [ -f /app/sandbox-data/update.sh ]; then
   /app/sandbox-data/update.sh
 fi
 
-sh -c 'sleep 30; /app/create_import_archive.sh ; /app/import_smockin.sh' &
+sh -c 'sleep 60; /app/create_import_archive.sh ; /app/import_smockin.sh' &
 
 cd /app/smockin
 ./run.sh -CONSOLE
