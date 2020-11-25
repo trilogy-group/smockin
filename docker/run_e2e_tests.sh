f6ca5014fc4ed50d8ef3c04c0bab5e0ea2560ade
Zbigniew Oględzki <zgibek@gmail.com>
2020-11-25 07:05:22 +0100

    running e2e with jenkins-cli job

--- docker/run_e2e_tests.sh
+++ docker/run_e2e_tests.sh
@@ -1,31 +1,37 @@
-outdir=/app/sandbox-tests/e2e-results
+export authsecret=zogledzki:1142d4c784e3933c8f5447663a640ffce2
+outdir=./e2e-results
 mkdir -p $outdir
 rm -rf $outdir/*
-mkdir $outdir/generated-features
-mkdir $outdir/tickets
 mkdir $outdir/results
+global_results=$outdir/results/statuses.txt
+echo '"Feature","FAILED","BROKEN","PASSED","SKIPPED","UNKNOWN"' > $global_results
 while IFS= read -r ticket
 do
     if [ "$ticket" != "" ] ; then
         echo "Processing with ticket: $ticket"
 
-        e2e-fetch --ticket $ticket --output-folder $outdir/generated-features
+        java -jar jenkins-cli.jar \
+          -s http://qa-integration-ci.jenkins.aureacentral.com/ \
+          -auth $authsecret \
+          build Feature/gherkin-execution-for-feature \
+          -s -v \
+          -p JIRA_ID=$ticket > $outdir/results/$ticket.output < /dev/null
 
-        if [ -f $outdir/generated-features/${ticket}.feature ]; then
-            # Change dynamically env to run on
-            dest_env=dev_zibi
-            sed "s|\(Given       environment\) \"[^\"]*\" \(from.*$\)|\1 \"$dest_env\" \2|g" \
-                $outdir/generated-features/$ticket.feature > $outdir/tickets/$ticket.feature
+      jobId=`grep "Completed Feature » gherkin-execution-for-feature" $outdir/results/$ticket.output | cut -d'#' -f 2 | cut -d' ' -f1`
 
-            # run test
-            e2e-behave $outdir/tickets/$ticket.feature \
-               --summary \
-               --outfile $outdir/results/$ticket.output.txt \
-               --logging-location $outdir/results
-        else
-            echo "Can't fetch feature $ticket'"
-            exit 1
-        fi
+      curl --silent --user $authsecret http://qa-integration-ci.jenkins.aureacentral.com/job/Feature/job/gherkin-execution-for-feature/$jobId/allure/data/behaviors.csv \
+      >  $outdir/results/$ticket.status
+      grep AWS $outdir/results/$ticket.status | cut -d',' -f2,4- >> $global_results
     fi
-done < /app/sandbox-tests/e2e-tickets.txt
+done < /tmp/smockin_export/e2e-tickets.txt
 
+cat $global_results
+
+status=`cat e2e-results/results/statuses.txt | cut -d',' -f2-3 | grep  '"1"' | wc -l`
+if [[ $status -gt 0 ]]; then
+    echo "Failure, at least one test failed or got broken! Number of failures/brokes: $status"
+    exit 1
+else
+    echo "Success, None test failed nor got broken!"
+    exit 0
+fi
